4f18c55f5e76bd9d399860e81c051cf83ba6854d
ayushkevich <yushkevich@me.com>
2017-10-05 11:06:48 +0300

    Download new template

---
+++ js/script.js
@@ -0,0 +1,91 @@
+
+jQuery(document).ready(function() {
+	
+	$(window).load(function () {
+		$(".loaded").fadeOut();
+		$(".preloader").delay(1000).fadeOut("slow");
+	});
+	jQuery(window).scroll(function () {
+	  var top = jQuery(document).scrollTop();
+		var height = 300;
+	  //alert(batas);
+	  
+	  if (top > height) {
+		jQuery('.navbar-fixed-top').addClass('menu-scroll');
+	  } else {
+	   jQuery('.navbar-fixed-top').removeClass('menu-scroll');
+	  }
+	});	
+	  
+	  
+	
+
+//<!-- =============================================== -->
+//<!-- ========== scrollTop.js ========== -->
+//<!-- =============================================== -->
+    
+$('.scrollup').click(function(){
+$("html, body").animate({ scrollTop: 0 }, 2000);
+return false;
+});
+
+
+
+
+//<!-- =============================================== -->
+//<!-- ========== scrollTop.js ========== -->
+//<!-- =============================================== -->
+    
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 600) {
+            $('.scrollup').fadeIn('slow');
+        } else {
+            $('.scrollup').fadeOut('slow');
+        }
+    });
+    $('.scrollup').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 800);
+        return false;
+    });
+			
+//<!-- =============================================== -->
+//<!-- ========== bootstrap scrollspy ========== -->
+//<!-- =============================================== -->
+    
+    
+    $('body').scrollspy({
+    	target: '.navbar',
+    	offset: 160
+    });
+
+//<!-- =============================================== -->
+//<!-- ========== navbardown.js ========== -->
+//<!-- =============================================== -->
+
+$('.nav a').bind('click', function () {
+    $('html , body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top - 80
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+});			
+			
+			
+
+ new WOW().init();
+
+
+		
+		
+});
+
+
+
+
+
+
+		
+  
+ 
+
+
+	
